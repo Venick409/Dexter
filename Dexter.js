a0deaef3890a48f759934ee431bd35f3ecfa4144
Venick <Venick409@gmail.com>
2017-02-14 13:29:17 -0600

    Changes how text is handled.

--- Dexter.js
+++ Dexter.js
@@ -1,14 +1,33 @@
 var Discord = require('discord.js');
 
 const auth = require('./auth.json');
+const likes = require('./likes.json');
 
 // Start bot
 var bot = new Discord.Client();
 bot.login(auth.token);
 
-// Bot send message
+/***
+ * Message read
+ ***/
+
 bot.on('message', msg => {
-    if (msg.content === 'test') {
-        msg.channel.sendMessage('Booyah!');
+
+	// Get the user's message
+	let txt = msg.content;
+	// Get each word of command in an array
+	let args = txt.split(' ');
+
+	// Convenience function for sending messages
+    function say(x) {
+        msg.channel.sendMessage(x);
     }
+
+    //Text Tests
+	if (txt === 'test'){
+		say('Booyah!');
+	} else if (args[0] === '.SV'){
+		say('testtest');
+	}
+
 });
