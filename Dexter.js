74e29608469d0f674225db02278e90ba66bf7400
Venick <Venick409@gmail.com>
2017-02-21 04:12:04 -0600

    added todo list test, still needs work.

--- Dexter.js
+++ Dexter.js
@@ -1,8 +1,9 @@
 var Discord = require('discord.js');
 var fs = require("fs");
-var contents = JSON.parse(fs.readFileSync("./likes.json"));
-6
+var parse = JSON.parse(fs.readFileSync('./likes.json'));
+var contents = JSON.parse(fs.readFileSync('./likes.json')); // Read likes.json
 const auth = require('./auth.json');
+var todoList = JSON.parse(fs.readFileSync('./todo.json')); // Read todo.json
 
 // Start bot
 var bot = new Discord.Client();
@@ -15,26 +16,42 @@ bot.login(auth.token);
 
 bot.on('message', msg => {
 
-	// Get the user's message
-	let txt = msg.content;
-
-	// Get each word of command in an array
-	let args = txt.split(' ');
-
+	let txt = msg.content; // Get the user's message
+	let args = txt.split(' '); // Turn user's message into an array
+	const defined = contents[args[1]] !== undefined // shorthand to check if undefined
 
 	// Convenience function for sending messages
     function say(x) {
         msg.channel.sendMessage(x);
     }
 
+    function todomsg(){
+    	let todo = '```';
+    	for (let i = 0; i < todoList.length; i++){
+    		todo += '\n' + (i + 1) +  ':' + todoList[i]
+    	}
+    	todo += '\n```';
+    	say(todo)
+}
+	function todoadd(x){
+		todoList.push(x)
+		say(x + ' added to todo list.')
+}
+
 
     //Text Tests
 	if (txt === 'test'){
 		say('Booyah!');
-	}else if (args[0] === '.sv' && contents[args[1]] !== undefined){
+	}else if (args[0] === '.sv' && defined){
 		say(contents[args[1]].villager + contents[args[1]].birthday + contents[args[1]].loves + contents[args[1]].likes);
-	}else if (contents[args[1]] === "villagers"){
+	}else if (contents[args[1]] === 'villagers'){
 		say(cpmtemts[args[1]].bachelors + '' + cpmtemts[args[1]].bachelorettes + '' + cpmtemts[args[1]].villagers);
-	}else if (txt === 'Dexter')
-		say("I'm Dexter, a Pokédex programmed by Professor Oak for Pokémon Trainer Ash Ketchum of the town of Pallet. My function is to provide Ash with information and advice regarding Pokémon and their training. If lost or stolen, I cannot be replaced.")
+	}else if (txt.toLowerCase() === 'dexter'){
+		say('I\'m Dexter, a Pokédex programmed by Professor Oak for Pokémon Trainer Ash Ketchum of the town of Pallet. My function is to provide Ash with information and advice regarding Pokémon and their training. If lost or stolen, I cannot be replaced.');
+	}else if (args[0] === '.todo'){
+		todomsg();
+	}else if (args[0] === '.todoadd'){
+		todoadd(args[1]);
+	}
+
 });
